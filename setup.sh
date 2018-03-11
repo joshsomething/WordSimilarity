2fe7091308e6d05704569b33d1bb2fc53f9f5ad8
Josh <joshprogramming@gmail.com>
2018-03-11 19:04:05 -0400

    Fix license notice
    Whoops, that’s not python! XD

--- setup.sh
+++ setup.sh
@@ -1,22 +1,21 @@
-'''	Copyright 2018 Joshua Danielpour
+#Copyright 2018 Joshua Danielpour
 
+# This file is part of WordSimilarity.
 
-	This file is part of WordSimilarity.
+#    WordSimilarity is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-    WordSimilarity is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+#    WordSimilarity is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 
-    WordSimilarity is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with WordSimilarity.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#  along with WordSimilarity.  If not, see <http://www.gnu.org/licenses/>.
     
-'''
+
 #!/bin/bash
 
 source bin/activate
